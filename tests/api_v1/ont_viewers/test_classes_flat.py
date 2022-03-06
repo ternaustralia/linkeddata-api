b64d4aebec97bf1f4fa6b756843d97a09e45b0b7
Edmond Chuc <e.chuc@uq.edu.au>
2022-03-06 16:37:35 +1000

    Add test for GET /api/v1.0/classes/flat

---
+++ tests/api_v1/ont_viewers/test_classes_flat.py
@@ -0,0 +1,9 @@
+from flask.testing import FlaskClient
+
+
+def test_classes_flat(client: FlaskClient):
+    response = client.get(
+        "/api/v1.0/classes/flat", query_string={"id": "tern-ontology"}
+    )
+    data = response.json
+    assert len(data) >= 30
