6ffb4888f3cef2aa1a2c394adae3c1d035e0c508
Edmond Chuc <e.chuc@uq.edu.au>
2022-03-06 23:36:33 +1000

    Update comment

--- src/linkeddata_api/pydantic_jsonify.py
+++ src/linkeddata_api/pydantic_jsonify.py
@@ -11,7 +11,7 @@ def jsonify(
     status: int = 200,
     mimetype: str = "application/json",
 ) -> Response:
-    """Return a Flask Response that uses pydantic_encoder to perform the Python object to JSON string."""
+    """Return a Flask Response that uses pydantic_encoder with json.dumps()."""
 
     return Response(
         json.dumps(data, indent=2, default=pydantic_encoder),
