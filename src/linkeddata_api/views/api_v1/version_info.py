a2eb7550e2892767c31bcd2bcea77294329b8f64
Edmond Chuc <e.chuc@uq.edu.au>
2022-03-07 11:47:53 +1000

    Add app version to container image. Read version from Flask app config.

--- src/linkeddata_api/views/api_v1/version_info.py
+++ src/linkeddata_api/views/api_v1/version_info.py
@@ -1,10 +1,10 @@
+from flask import current_app
 from flask_tern import openapi
 
 from .blueprint import bp
-from linkeddata_api.version import version
 
 
 @bp.route("/version")
 @openapi.validate(validate_response=False)
 def version_get():
-    return version
+    return current_app.config["VERSION"]
