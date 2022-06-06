841d5a275fc00f63039bdbf7629bb6d27467c469
Edmond Chuc <e.chuc@uq.edu.au>
2022-06-06 03:51:38 +0000

    Remove cookiecutter example code

--- src/linkeddata_api/models/example.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-
-class Example(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    count = db.Column(db.Integer, nullable=False)
--- src/linkeddata_api/views/api_v1/__init__.py
+++ src/linkeddata_api/views/api_v1/__init__.py
@@ -1,6 +1,6 @@
 from .blueprint import bp
 
 # import all sub modules with views registered with blueprint
-from . import example
 from . import ontology_viewer
+from . import vocab_viewer
 from . import version_info
