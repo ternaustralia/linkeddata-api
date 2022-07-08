baf60618bcbe5d76c7baf75280ee19f5f81cdb3d
Edmond Chuc <e.chuc@uq.edu.au>
2022-07-08 01:46:04 +0000

    Remove prints and dead code

--- src/linkeddata_api/vocab_viewer/nrm/curie.py
+++ src/linkeddata_api/vocab_viewer/nrm/curie.py
@@ -1,7 +1,5 @@
 import requests
 
-# from linkeddata_api.vocab_viewer import nrm
-
 # TODO: Improve this, use a cache lib
 # TODO: Serve cache if time limit reached but still make request
 cache = {}
@@ -21,13 +19,10 @@ def uri_in_skips(uri: str) -> bool:
 # TODO: use async?
 def get(uri: str):
     if uri in cache:
-        # print("cache hit!")
         return cache.get(uri)
     if uri_in_skips(uri):
-        # print("skipping!")
         return uri
 
-    # print("sending request")
     response = requests.post(
         "https://prefix.zazuko.com/api/v1/shrink", params={"q": uri}
     )
@@ -35,12 +30,9 @@ def get(uri: str):
     try:
         response.raise_for_status()
     except requests.RequestException as err:
-        # raise nrm.exceptions.RequestError(err.response.text) from err
         cache[uri] = uri
-        # print("done")
         return uri
 
-    # print("done")
     value = response.json()["value"]
     cache[uri] = value
     return value
