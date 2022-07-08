6afa84ec131280e88d857dabeb7b484d2b0a8781
Edmond Chuc <e.chuc@uq.edu.au>
2022-07-08 05:09:51 +0000

    Add resource as an nrm endpoint

--- src/linkeddata_api/views/api_v1/vocab_viewer/nrm/__init__.py
+++ src/linkeddata_api/views/api_v1/vocab_viewer/nrm/__init__.py
@@ -1,1 +1,2 @@
 from . import vocabs
+from . import resource
---
+++ src/linkeddata_api/views/api_v1/vocab_viewer/nrm/resource.py
@@ -0,0 +1,19 @@
+from flask import request
+from flask_tern import openapi
+from flask_tern.logging import create_audit_event, log_audit
+from werkzeug.exceptions import HTTPException
+from werkzeug.wrappers import Response
+
+from linkeddata_api.pydantic_jsonify import jsonify
+from linkeddata_api.views.api_v1.blueprint import bp
+from linkeddata_api.vocab_viewer import nrm
+
+
+@bp.get("/vocab_viewer/nrm/resource")
+@openapi.validate(validate_response=False)
+def get_nrm_resource():
+    uri = request.args.get("uri")
+
+    result = nrm.resource.get(uri)
+
+    return jsonify(result, headers={"cache-control": "max-age=600, s-maxage=3600"})
